40ccd3b0be29e27463cbfb7a63bfae838af9dca2
Jack <actionjacks22@gmail.com>
2022-03-17 22:14:15 +0100

    add header,aboutMe

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="main_wiew">
     <ThemeSwitcher />
+    <Header />
+
+    <AboutMe />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import ThemeSwitcher from "@/components/ThemeSwitcher.vue";
+import Header from "@/components/Header.vue";
+import AboutMe from "@/components/AboutMe.vue";
 
 export default defineComponent({
-  components: { ThemeSwitcher },
+  components: { ThemeSwitcher, Header, AboutMe },
   setup() {
     return {};
   },
